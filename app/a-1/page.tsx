326f98403f9df79a21c19017cd91285c49cd7d94
shalker760 <shalker760@gmail.com>
2025-06-18 11:27:02 +0300

    Update page.tsx

--- app/a-1/page.tsx
+++ app/a-1/page.tsx
@@ -17,8 +17,6 @@ export default function event1() {
       </div>
       <div className="absolute bottom-[5%] left-[50%] translate-x-[-50%] py-2 text-2xl">
         <div onClick={() => router.push("/start")}> 1 </div>
-        <div onClick={() => router.push("/start")}> 2 </div>
-        <div onClick={() => router.push("/start")}> 3 </div>
         
       </div>
     </>
