b8fb383ae970c15695a896e1dbf39e7f2f87d5de
shalker760 <shalker760@gmail.com>
2025-06-19 16:37:13 +0000

    испарв

--- app/continue/jdy.tsx
+++ app/continue/jdy.tsx
@@ -10,7 +10,7 @@ export default function event1() {
     <>
       <div className='py-2 text-5xl'>
       <TypingText duration={500}>
-       это пока все дальше жди
+        это пока все дальше жди
         </TypingText>
       </div>
       <div className="absolute bottom-[5%] left-[50%] translate-x-[-50%] py-2 text-2xl">
